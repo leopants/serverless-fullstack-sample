d96221871414e130cab9d76731d5579a68aa25ef
Leo Pantaleon <leopanta99@gmail.com>
2024-06-16 18:08:32 -0400

    finishing portfolio site with dark mode/shadcn/icons

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -1,112 +1,161 @@
+"use client";
+import { useState } from "react";
 import Image from "next/image";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Sun, Moon, Github, Linkedin, Mail } from "lucide-react";
+import { ThemeProvider } from "@/components/theme-provider";
+import { useTheme } from "next-themes";
+import profilePic from "./profile.jpg";
 
 export default function Home() {
+  const { theme, setTheme } = useTheme();
+
+  const changeThemeColor = () => {
+    if (theme == "light") {
+      setTheme("dark");
+    } else {
+      setTheme("light");
+    }
+    console.log(theme);
+  };
+
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
+      <div className="grid grid-cols-5  gap-4 z-10 w-full max-w-5xl items-center justify-between text-sm">
+        <Card className="col-span-2 dark:border-neutral-800 dark:bg-zinc-800/30  border-neutral-300 bg-zinc-300/20 h-full w-full">
+          <CardHeader className="pb-2">
+            <CardTitle className="text-2xl">Hi, I'm Leo Pantaleon</CardTitle>
+          </CardHeader>
+          <CardContent className="mt-3">
+            <p>Full Stack Software Engineer</p>
+            <p>Capital One</p>
+            <p>Boston, MA</p>
+          </CardContent>
+        </Card>
+        <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
+          <CardContent className="flex justify-center p-0">
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+              src={profilePic}
+              width={200}
+              height={200}
+              alt="Picture of the author"
+              className="rounded-xl"
             />
-          </a>
+          </CardContent>
+        </Card>
+        <Card
+          className="cursor-pointer dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full"
+          onClick={() => changeThemeColor()}
+        >
+          <CardHeader className="pb-2">
+            <CardTitle>
+              {theme == "light" && (
+                <Moon className="h-6 w-6 justify-self-center transition-all group-hover:scale-110" />
+              )}
+              {theme == "dark" && (
+                <Sun className="h-6 w-6 justify-self-center transition-all group-hover:scale-110" />
+              )}
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="flex justify-center items-center p-6">
+            {theme == "light" && <p>Click to turn off the lights</p>}
+            {theme == "dark" && <p>Click to turn on the lights</p>}
+          </CardContent>
+        </Card>
+        <div>
+          <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 mb-2">
+            <CardContent className="flex justify-center p-4">
+              <Github className="h-6 w-6 justify-self-center transition-all group-hover:scale-110" />
+            </CardContent>
+          </Card>
+          <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 mb-2">
+            <CardContent className="flex justify-center p-4">
+              <Linkedin className="h-6 w-6 justify-self-center transition-all group-hover:scale-110" />
+            </CardContent>
+          </Card>
+          <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20">
+            <CardContent className="flex justify-center p-4">
+              <Mail className="h-6 w-6 justify-self-center transition-all group-hover:scale-110" />
+            </CardContent>
+          </Card>
         </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
+        <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 col-span-5 mt-4">
+          <CardContent className="p-4">
+            <p>Experience</p>
+          </CardContent>
+        </Card>
+        <Card className="col-span-3 dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
+          <CardHeader className="pb-2">
+            <CardTitle className="text-2xl">Capital One</CardTitle>
+          </CardHeader>
+          <CardContent className="mt-3">
+            <p>Full Stack Software Engineer</p>
+            <p>Boston, MA</p>
+          </CardContent>
+        </Card>
+        <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
+          <CardHeader className="pb-2">
+            <CardTitle className="text-2xl">Deloitte</CardTitle>
+          </CardHeader>
+          <CardContent className="mt-3">
+            <p>Solutions Analyst</p>
+            <p>Lake Mary, FL</p>
+          </CardContent>
+        </Card>
+        <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
+          <CardHeader className="pb-2">
+            <CardTitle className="text-2xl">FIU</CardTitle>
+          </CardHeader>
+          <CardContent className="mt-3">
+            <p>Research Assistant</p>
+            <p>Miami, FL</p>
+          </CardContent>
+        </Card>
+        <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 col-span-5 mt-4">
+          <CardContent className="p-4">
+            <p>Projects</p>
+          </CardContent>
+        </Card>
+        <Card className="col-span-3 dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
+          <CardHeader className="pb-2">
+            <CardTitle className="text-2xl">Saitemap.com</CardTitle>
+          </CardHeader>
+          <CardContent className="mt-3">
+            <p>
+              AI Powered Content Generation System based around a simple
+              concept. All sites have sitemaps and they're easier to think up
+              than all the content for your site, so give us a sitemap we'll
+              create the content
+            </p>
+            <p>Still working to make this great!</p>
+          </CardContent>
+        </Card>
+        <Card className="col-span-2 dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
+          <CardHeader className="pb-2">
+            <CardTitle className="text-2xl">
+              SparkDev Transit Alliance Team{" "}
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="mt-3">
+            <p>
+              Built a backend for a web application commissioned by Miami-Dade
+              Transit to guide citizens through a transit system revamp which
+              went into effect in 2020.
+            </p>
+            <p>
+              Built with the support of Microsoft and presented to the Miami
+              Dade City Council.
+            </p>
+          </CardContent>
+        </Card>
       </div>
     </main>
   );
