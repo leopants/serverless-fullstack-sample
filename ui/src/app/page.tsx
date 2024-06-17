0fc478ab2de3e0388bde471331387d973f7feedf
Leo Pantaleon <leopanta99@gmail.com>
2024-06-16 21:45:25 -0400

    adding links to portfolio ui

--- ui/src/app/page.tsx
+++ ui/src/app/page.tsx
@@ -10,10 +10,16 @@ import {
   CardTitle,
 } from "@/components/ui/card";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-import { Sun, Moon, Github, Linkedin, Mail } from "lucide-react";
+import { Sun, Moon, Github, Linkedin, Mail, ExternalLink } from "lucide-react";
 import { ThemeProvider } from "@/components/theme-provider";
 import { useTheme } from "next-themes";
 import profilePic from "./profile.jpg";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export default function Home() {
   const { theme, setTheme } = useTheme();
@@ -29,8 +35,8 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="grid grid-cols-5  gap-4 z-10 w-full max-w-5xl items-center justify-between text-sm">
-        <Card className="col-span-2 dark:border-neutral-800 dark:bg-zinc-800/30  border-neutral-300 bg-zinc-300/20 h-full w-full">
+      <div className="grid grid-cols-10  gap-4 z-10 w-full max-w-5xl items-center justify-between text-sm">
+        <Card className="col-span-4 dark:border-neutral-800 dark:bg-zinc-800/30  border-neutral-300 bg-zinc-300/20 h-full w-full">
           <CardHeader className="pb-2">
             <CardTitle className="text-2xl">
               Hi, I&apos;m Leo Pantaleon
@@ -42,22 +48,22 @@ export default function Home() {
             <p>Boston, MA</p>
           </CardContent>
         </Card>
-        <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
+        <Card className="col-span-2 dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
           <CardContent className="flex justify-center p-0">
             <Image
               src={profilePic}
               width={200}
               height={200}
               alt="Picture of the author"
-              className="rounded-xl"
+              className="rounded-xl contain"
             />
           </CardContent>
         </Card>
         <Card
-          className="cursor-pointer dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full"
+          className="col-span-2 cursor-pointer dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full"
           onClick={() => changeThemeColor()}
         >
-          <CardHeader className="pb-2">
+          <CardHeader className="pb-2 ">
             <CardTitle>
               {theme == "light" && (
                 <Moon className="h-6 w-6 justify-self-center transition-all group-hover:scale-110" />
@@ -72,63 +78,98 @@ export default function Home() {
             {theme == "dark" && <p>Click to turn on the lights</p>}
           </CardContent>
         </Card>
-        <div>
+        <div className="col-span-2">
           <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 mb-2">
-            <CardContent className="flex justify-center p-4">
-              <Github className="h-6 w-6 justify-self-center transition-all group-hover:scale-110" />
-            </CardContent>
+            <a href="https://github.com/leopants">
+              <CardContent className="flex justify-center p-4">
+                <Github className="h-6 w-6 justify-self-center transition-all group-hover:scale-110" />
+              </CardContent>
+            </a>
           </Card>
           <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 mb-2">
-            <CardContent className="flex justify-center p-4">
-              <Linkedin className="h-6 w-6 justify-self-center transition-all group-hover:scale-110" />
-            </CardContent>
+            <a href="https://linkedin.com/in/leopants">
+              <CardContent className="flex justify-center p-4">
+                <Linkedin className="h-6 w-6 justify-self-center transition-all group-hover:scale-110" />
+              </CardContent>
+            </a>
           </Card>
           <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20">
-            <CardContent className="flex justify-center p-4">
-              <Mail className="h-6 w-6 justify-self-center transition-all group-hover:scale-110" />
-            </CardContent>
+            <TooltipProvider>
+              <Tooltip>
+                <CardContent
+                  onClick={() => {
+                    navigator.clipboard.writeText("leopantaleon@outlook.com");
+                  }}
+                  className="cursor-pointer flex justify-center p-4"
+                >
+                  <TooltipTrigger>
+                    <Mail className="h-6 w-6 justify-self-center transition-all group-hover:scale-110" />
+                  </TooltipTrigger>
+                </CardContent>
+                <TooltipContent className="mb-2">
+                  <p>Copy Email</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
           </Card>
         </div>
-        <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 col-span-5 mt-4">
+        <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 col-span-10 mt-4">
           <CardContent className="p-4">
             <p>Experience</p>
           </CardContent>
         </Card>
-        <Card className="col-span-3 dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
+        <Card className="col-span-4 dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
           <CardHeader className="pb-2">
             <CardTitle className="text-2xl">Capital One</CardTitle>
           </CardHeader>
           <CardContent className="mt-3">
             <p>Full Stack Software Engineer</p>
             <p>Boston, MA</p>
+            <p className="mt-1">
+              Have spent my time building a service to automate journal entry
+              submissions onto the banks general ledger, among other things
+            </p>
           </CardContent>
         </Card>
-        <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
+        <Card className="col-span-3 dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
           <CardHeader className="pb-2">
             <CardTitle className="text-2xl">Deloitte</CardTitle>
           </CardHeader>
           <CardContent className="mt-3">
             <p>Solutions Analyst</p>
             <p>Lake Mary, FL</p>
+            <p className="mt-1">
+              Managed 3 out of the 7 stages of the Marine Corps purchasing
+              system running on an ESB
+            </p>
           </CardContent>
         </Card>
-        <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
+        <Card className="col-span-3 dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
           <CardHeader className="pb-2">
             <CardTitle className="text-2xl">FIU</CardTitle>
           </CardHeader>
           <CardContent className="mt-3">
             <p>Research Assistant</p>
             <p>Miami, FL</p>
+            <p className="mt-1">
+              Worked with Autonomous Underwater Vehicles to take water samples
+              around Biscayne Bay
+            </p>
           </CardContent>
         </Card>
-        <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 col-span-5 mt-4">
+        <Card className="dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 col-span-10 mt-4">
           <CardContent className="p-4">
             <p>Projects</p>
           </CardContent>
         </Card>
         <Card className="col-span-3 dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
           <CardHeader className="pb-2">
-            <CardTitle className="text-2xl">Saitemap.com</CardTitle>
+            <CardTitle className="flex justify-between">
+              <p className="text-2xl">Saitemap.com</p>
+              <a href="https://saitemap.com">
+                <ExternalLink className="h-5 w-5 justify-self-center transition-all group-hover:scale-110" />
+              </a>
+            </CardTitle>
           </CardHeader>
           <CardContent className="mt-3">
             <p>
@@ -140,7 +181,7 @@ export default function Home() {
             <p>Still working to make this great!</p>
           </CardContent>
         </Card>
-        <Card className="col-span-2 dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
+        <Card className="col-span-4 dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
           <CardHeader className="pb-2">
             <CardTitle className="text-2xl">
               SparkDev Transit Alliance Team{" "}
@@ -155,6 +196,24 @@ export default function Home() {
             <p>
               Built with the support of Microsoft and presented to the Miami
               Dade City Council.
+            </p>
+          </CardContent>
+        </Card>
+        <Card className="col-span-3 dark:border-neutral-800 dark:bg-zinc-800/30 border-neutral-300 bg-zinc-300/20 h-full w-full">
+          <CardHeader className="pb-2">
+            <CardTitle className="text-2xl"></CardTitle>
+            <CardTitle className="flex justify-between">
+              <p className="text-2xl">This Portfolio</p>
+              <a href="https://github.com/leopants/serverless-fullstack-sample">
+                <ExternalLink className="h-5 w-5 justify-self-center transition-all group-hover:scale-110" />
+              </a>
+            </CardTitle>
+          </CardHeader>
+          <CardContent className="mt-3">
+            <p>
+              This portfolio is my actually my template to bootstrap serverless
+              full stack development of personal projects, it&apos;s all on
+              Github
             </p>
           </CardContent>
         </Card>
